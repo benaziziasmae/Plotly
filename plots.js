adb4813b23e698d6cd045c63256c2b1223b0fc76
unknown <you@example.com>
2021-03-17 18:07:27 -0400

    plotly adding

---
+++ plots.js
@@ -0,0 +1,21 @@
+X: 5,10,15. Y: 10,20,30
+Plotly.newPlot(“plotArea”, [{x:[5,10,15],y:[10,20,30]}]);
+
+
+var topFiveCityNames = topFiveCities.map(city => city.City);
+var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
+
+var trace = {
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: { title: "City" },
+    yaxis: { title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot", data, layout);
+
+  
